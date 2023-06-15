proxmox/pve-installer

commit 86dac562e824fc024d6d8ee02672275756e4d8e4

tui: use whole keyboard mapping in select view as value
Thus all its values can easily be used later on. Involves making
`KeyboardMapping` partial-orderable, just use the human-readable name
here (as that's what displayed to the user).

Signed-off-by: Christoph Heiss <c.heiss@proxmox.com>

proxmox-tui-installer/src/setup.rs
@@ -1,4 +1,4 @@
-use std::{collections::HashMap, fs::File, io::BufReader, path::Path};
+use std::{cmp, collections::HashMap, fs::File, io::BufReader, path::Path};
 
 use serde::{Deserialize, Deserializer};
 
@@ -41,15 +41,21 @@ pub struct CountryInfo {
     pub kmap: String,
 }
 
-#[derive(Clone, Deserialize)]
+#[derive(Clone, Deserialize, Eq, PartialEq, Ord)]
 pub struct KeyboardMapping {
     pub name: String,
     #[serde(rename = "kvm")]
     pub id: String,
     #[serde(rename = "x11")]
-    pub layout: String,
+    pub xkb_layout: String,
     #[serde(rename = "x11var")]
-    pub variant: String,
+    pub xkb_variant: String,
+}
+
+impl cmp::PartialOrd for KeyboardMapping {
+    fn partial_cmp(&self, other: &Self) -> Option<cmp::Ordering> {
+        self.name.partial_cmp(&other.name)
+    }
 }
 
 #[derive(Clone, Deserialize)]

proxmox-tui-installer/src/views/timezone.rs
@@ -1,5 +1,8 @@
 use super::FormView;
-use crate::{options::TimezoneOptions, setup::LocaleInfo};
+use crate::{
+    options::TimezoneOptions,
+    setup::{KeyboardMapping, LocaleInfo},
+};
 use cursive::{
     view::{Nameable, ViewWrapper},
     views::{NamedView, SelectView},
@@ -48,13 +51,13 @@ impl TimezoneOptionsView {
             .kmap
             .clone()
             .into_values()
-            .map(|l| (l.name, l.id))
-            .collect::<Vec<(String, String)>>();
+            .map(|l| (l.name.clone(), l))
+            .collect::<Vec<(String, KeyboardMapping)>>();
         kb_layouts.sort();
 
         let kb_layout_selected_pos = kb_layouts
             .iter()
-            .position(|l| l.1 == options.kb_layout)
+            .position(|l| l.1.id == options.kb_layout)
             .unwrap_or_default();
 
         let view = FormView::new()
@@ -86,15 +89,15 @@ impl TimezoneOptionsView {
             .get_value::<NamedView<SelectView>, _>(1)
             .ok_or("failed to retrieve timezone")?;
 
-        let kb_layout = self
+        let kmap = self
             .view
-            .get_value::<SelectView, _>(2)
+            .get_value::<SelectView<KeyboardMapping>, _>(2)
             .ok_or("failed to retrieve keyboard layout")?;
 
         Ok(TimezoneOptions {
             country,
             timezone,
-            kb_layout,
+            kb_layout: kmap.id,
         })
     }
 

commit 1353d6cf91995d26274181eac8cc1dee2f6c5e0f

tui: implement both `PartialOrd` and `Ord` for `KeyboardMapping`
Otherwise, clippy (correctly) complains that you should always implement
both if you implement one.

Fixes: 86dac56 ("tui: use whole keyboard mapping in select view as value")
Signed-off-by: Christoph Heiss <c.heiss@proxmox.com>

proxmox-tui-installer/src/setup.rs
@@ -41,7 +41,7 @@ pub struct CountryInfo {
     pub kmap: String,
 }
 
-#[derive(Clone, Deserialize, Eq, PartialEq, Ord)]
+#[derive(Clone, Deserialize, Eq, PartialEq)]
 pub struct KeyboardMapping {
     pub name: String,
     #[serde(rename = "kvm")]
@@ -55,6 +55,12 @@ pub struct KeyboardMapping {
 impl cmp::PartialOrd for KeyboardMapping {
     fn partial_cmp(&self, other: &Self) -> Option<cmp::Ordering> {
         self.name.partial_cmp(&other.name)
+    }
+}
+
+impl cmp::Ord for KeyboardMapping {
+    fn cmp(&self, other: &Self) -> cmp::Ordering {
+        self.name.cmp(&other.name)
     }
 }
 