proxmox/pve-installer

commit 7f273738d77c55a3bf2c8d2c9e249051f4d3b0d3

tui: add notices for zfs/btrfs bootdisk and their caveats
Signed-off-by: Christoph Heiss <c.heiss@proxmox.com>

proxmox-tui-installer/src/views/bootdisk.rs
@@ -6,7 +6,7 @@ use crate::options::{
 use cursive::{
     theme::Effect,
     view::{Nameable, Resizable, ViewWrapper},
-    views::{Button, Dialog, DummyView, LinearLayout, NamedView, SelectView, TextView},
+    views::{Button, Dialog, DummyView, LinearLayout, NamedView, Panel, SelectView, TextView},
     Cursive, View,
 };
 use std::{cell::RefCell, marker::PhantomData, rc::Rc};
@@ -283,6 +283,14 @@ impl<T: View> MultiDiskOptionsView<T> {
         }
     }
 
+    fn top_panel(mut self, view: impl View) -> Self {
+        self.view = LinearLayout::vertical()
+            .child(Panel::new(view))
+            .child(self.view);
+
+        self
+    }
+
     fn get_disks(&mut self) -> Option<Vec<Disk>> {
         let mut disks = vec![];
         let disk_form = self
@@ -324,7 +332,8 @@ impl BtrfsBootdiskOptionsView {
         let view = MultiDiskOptionsView::new(
             disks,
             FormView::new().child("hdsize", DiskSizeEditView::new().content(options.disk_size)),
-        );
+        )
+        .top_panel(TextView::new("Btrfs integration is a technology preview!").center());
 
         Self { view }
     }
@@ -377,9 +386,12 @@ impl ZfsBootdiskOptionsView {
             .child("copies", IntegerEditView::new().content(options.copies))
             .child("hdsize", DiskSizeEditView::new().content(options.disk_size));
 
-        Self {
-            view: MultiDiskOptionsView::new(disks, inner),
-        }
+        let view = MultiDiskOptionsView::new(disks, inner)
+            .top_panel(TextView::new(
+                "ZFS is not compatible with hardware RAID controllers, for details see the documentation."
+            ).center());
+
+        Self { view }
     }
 
     fn get_values(&mut self) -> Option<(Vec<Disk>, ZfsBootdiskOptions)> {

commit 82cc9fc4802de467688be3d07e43f77233d95a29

tui: fix bootdisk dialog for zfs/btrfs
This was broken when adding the usage caveats at the top of the advanced
dialog; the wrong child views were selected.

Fixes: 7f27373 ("tui: add notices for zfs/btrfs bootdisk and their caveats")
Signed-off-by: Christoph Heiss <c.heiss@proxmox.com>

proxmox-tui-installer/src/views/bootdisk.rs
@@ -278,23 +278,28 @@ impl<T: View> MultiDiskOptionsView<T> {
             .child(options_view);
 
         Self {
-            view,
+            view: LinearLayout::vertical().child(view),
             phantom: PhantomData,
         }
     }
 
     fn top_panel(mut self, view: impl View) -> Self {
-        self.view = LinearLayout::vertical()
-            .child(Panel::new(view))
-            .child(self.view);
+        if self.has_top_panel() {
+            self.view.remove_child(0);
+        }
 
+        self.view.insert_child(0, Panel::new(view));
         self
     }
 
     fn get_disks(&mut self) -> Option<Vec<Disk>> {
         let mut disks = vec![];
+        let view_top_index = usize::from(self.has_top_panel());
+
         let disk_form = self
             .view
+            .get_child(view_top_index)?
+            .downcast_ref::<LinearLayout>()?
             .get_child(0)?
             .downcast_ref::<LinearLayout>()?
             .get_child(2)?
@@ -310,11 +315,22 @@ impl<T: View> MultiDiskOptionsView<T> {
     }
 
     fn inner_mut(&mut self) -> Option<&mut T> {
+        let view_top_index = usize::from(self.has_top_panel());
+
         self.view
+            .get_child_mut(view_top_index)?
+            .downcast_mut::<LinearLayout>()?
             .get_child_mut(2)?
             .downcast_mut::<LinearLayout>()?
             .get_child_mut(2)?
             .downcast_mut::<T>()
+    }
+
+    fn has_top_panel(&self) -> bool {
+        // The root view should only ever have one or two children
+        assert!([1, 2].contains(&self.view.len()));
+
+        self.view.len() == 2
     }
 }
 