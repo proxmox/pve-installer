18f58123e8c83672ac97e16e5fe24876d0720947
Christoph Heiss <c.heiss@proxmox.com>
2023-10-20 11:46:44 +0200

    net: move hostname/fqdn regexes into common code
    Such that they can be re-used by other parts.
    No functional changes.
    
    Signed-off-by: Christoph Heiss <c.heiss@proxmox.com>

--- Proxmox/Sys/Net.pm
+++ Proxmox/Sys/Net.pm
@@ -6,6 +6,9 @@ use warnings;
 use base qw(Exporter);
 our @EXPORT_OK = qw(parse_ip_address parse_ip_mask);
 
+our $HOSTNAME_RE = "(?:[a-zA-Z0-9](?:[a-zA-Z0-9\-]*[a-zA-Z0-9])?)";
+our $FQDN_RE = "(?:${HOSTNAME_RE}\.)*${HOSTNAME_RE}";
+
 my $IPV4OCTET = "(?:25[0-5]|(?:2[0-4]|1[0-9]|[1-9])?[0-9])";
 my $IPV4RE = "(?:(?:$IPV4OCTET\\.){3}$IPV4OCTET)";
 my $IPV6H16 = "(?:[0-9a-fA-F]{1,4})";
