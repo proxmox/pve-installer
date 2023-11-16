proxmox/pve-installer

commit a31259b1597447a0b431cd5c81a6db2bc80f1ddf

unconfigured.sh: set terminal size for serial install
Signed-off-by: Stoiko Ivanov <s.ivanov@proxmox.com>

unconfigured.sh
@@ -16,6 +16,9 @@ parse_cmdline() {
             proxtui)
                 proxtui=1
             ;;
+            console=ttyS*)
+                serial=1
+            ;;
         esac
     done;
 }
@@ -211,6 +214,11 @@ setsid /sbin/agetty -a root --noclear tty3 &
 /usr/bin/proxmox-low-level-installer dump-env
 
 if [ $proxtui -ne 0 ]; then
+    if [ "$serial" -ne 0 ]; then
+        echo "Setting terminal size to 80x24 for serial install"
+        stty columns 80 rows 25
+    fi
+
     echo "Starting the TUI installer"
     /usr/bin/proxmox-tui-installer 2>/dev/tty2
 else

commit e2cbcc4ec67e2ad5081bf952bc1afe52e378f259

unconfigured.sh: set serial to a number to prevent warning
caught me off-guard while debugging other things - the message:
` [: : integer expression expected`
made me look a bit too long at the wrong place.

Fixes: a31259b1597447a0b431cd5c81a6db2bc80f1ddf
Signed-off-by: Stoiko Ivanov <s.ivanov@proxmox.com>

unconfigured.sh
@@ -7,6 +7,7 @@ trap "err_reboot" ERR
 parse_cmdline() {
     proxdebug=0
     proxtui=0
+    serial=0
     # shellcheck disable=SC2013 # per word splitting is wanted here
     for par in $(cat /proc/cmdline); do
         case $par in