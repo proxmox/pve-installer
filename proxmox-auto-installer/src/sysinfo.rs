a160700b93d26f83645f9f34e6ce69cb2376cdf7
Thomas Lamprecht <t.lamprecht@proxmox.com>
2024-04-19 14:20:16 +0200

    auto installer: rework sys-info struct layout and add more details
    move the decoded DMI info into a "dmi" object to avoid crowding the
    outer layer, add more info from the ISO and Product currently booted
    and use the complete mocked variants for the assistant environments so
    that users evaluating this have a full picture of possible keys.
    
    Also include the NIC link name with the mach address so that one can
    also configure the management interface correctly from a MAC.
    
    Do this all in a slightly more ergonomic rust way, using separate
    structs and impls for getting the info.
    
    There could be even more details added, but that's for a future patch,
    this one mostly transforms and enhances existing keys.
    
    Signed-off-by: Thomas Lamprecht <t.lamprecht@proxmox.com>

--- proxmox-auto-installer/src/sysinfo.rs
+++ proxmox-auto-installer/src/sysinfo.rs
@@ -1,49 +1,29 @@
 use anyhow::{bail, Result};
-use proxmox_installer_common::setup::SetupInfo;
+use proxmox_installer_common::setup::{IsoInfo, ProductConfig, SetupInfo};
 use serde::Serialize;
-use std::{collections::HashMap, fs, io, path::Path};
+use std::{collections::HashMap, fs, io};
 
 use crate::utils::get_nic_list;
 
 const DMI_PATH: &str = "/sys/devices/virtual/dmi/id";
 
 pub fn get_sysinfo(pretty: bool) -> Result<String> {
-    let system_files = vec![
-        "product_serial",
-        "product_sku",
-        "product_uuid",
-        "product_name",
-    ];
-    let baseboard_files = vec!["board_asset_tag", "board_serial", "board_name"];
-    let chassis_files = vec!["chassis_serial", "chassis_sku", "chassis_asset_tag"];
+    let dmi = SystemDMI::get()?;
 
-    let system = get_dmi_infos(system_files)?;
-    let baseboard = get_dmi_infos(baseboard_files)?;
-    let chassis = get_dmi_infos(chassis_files)?;
-
-    let mut mac_addresses: Vec<String> = Vec::new();
-    let links = get_nic_list()?;
-    for link in links {
-        let address = fs::read_to_string(format!("/sys/class/net/{link}/address"))?;
-        let address = String::from(address.trim());
-        mac_addresses.push(address);
-    }
-
-    let iso_info = Path::new("/run/proxmox-installer/iso-info.json");
-    let mut product = String::from("Not available. Would be one of the following: pve, pmg, pbs");
-    if iso_info.exists() {
-        let file = fs::File::open("/run/proxmox-installer/iso-info.json")?;
-        let reader = io::BufReader::new(file);
-        let setup_info: SetupInfo = serde_json::from_reader(reader)?;
-        product = setup_info.config.product.to_string();
-    }
+    let setup_info: SetupInfo = match fs::File::open("/run/proxmox-installer/iso-info.json") {
+        Ok(iso_info_file) => {
+            let reader = io::BufReader::new(iso_info_file);
+            serde_json::from_reader(reader)?
+        }
+        Err(err) if err.kind() == io::ErrorKind::NotFound => SetupInfo::mocked(),
+        Err(err) => bail!("failed to open iso-info.json - {err}"),
+    };
 
     let sysinfo = SysInfo {
-        product,
-        system,
-        baseboard,
-        chassis,
-        mac_addresses,
+        product: setup_info.config,
+        iso: setup_info.iso_info,
+        network_interfaces: NetdevWithMac::get_all()?,
+        dmi,
     };
     if pretty {
         return Ok(serde_json::to_string_pretty(&sysinfo)?);
@@ -53,29 +33,75 @@ pub fn get_sysinfo(pretty: bool) -> Result<String> {
 
 #[derive(Debug, Serialize)]
 struct SysInfo {
-    product: String,
+    product: ProductConfig,
+    iso: IsoInfo,
+    dmi: SystemDMI,
+    network_interfaces: Vec<NetdevWithMac>,
+}
+
+#[derive(Debug, Serialize)]
+struct NetdevWithMac {
+    /// The network link name
+    pub link: String,
+    /// The MAC address of the network device
+    pub mac: String,
+}
+
+impl NetdevWithMac {
+    fn get_all() -> Result<Vec<Self>> {
+        let mut result: Vec<Self> = Vec::new();
+
+        let links = get_nic_list()?;
+        for link in links {
+            let mac = fs::read_to_string(format!("/sys/class/net/{link}/address"))?;
+            let mac = String::from(mac.trim());
+            result.push(Self { link, mac });
+        }
+        Ok(result)
+    }
+}
+
+#[derive(Debug, Serialize)]
+struct SystemDMI {
     system: HashMap<String, String>,
     baseboard: HashMap<String, String>,
     chassis: HashMap<String, String>,
-    mac_addresses: Vec<String>,
 }
 
-fn get_dmi_infos(files: Vec<&str>) -> Result<HashMap<String, String>> {
-    let mut res: HashMap<String, String> = HashMap::new();
+impl SystemDMI {
+    pub(crate) fn get() -> Result<Self> {
+        let system_files = vec![
+            "product_serial",
+            "product_sku",
+            "product_uuid",
+            "product_name",
+        ];
+        let baseboard_files = vec!["board_asset_tag", "board_serial", "board_name"];
+        let chassis_files = vec!["chassis_serial", "chassis_sku", "chassis_asset_tag"];
 
-    for file in files {
-        let path = format!("{DMI_PATH}/{file}");
-        let content = match fs::read_to_string(&path) {
-            Err(ref err) if err.kind() == std::io::ErrorKind::NotFound => continue,
-            Err(ref err) if err.kind() == std::io::ErrorKind::PermissionDenied => {
-                bail!("Could not read data. Are you running as root or with sudo?")
-            }
-            Err(err) => bail!("Error: '{err}' on '{path}'"),
-            Ok(content) => content.trim().into(),
-        };
-        let key = file.splitn(2, '_').last().unwrap();
-        res.insert(key.into(), content);
+        Ok(Self {
+            system: Self::get_dmi_infos(system_files)?,
+            baseboard: Self::get_dmi_infos(baseboard_files)?,
+            chassis: Self::get_dmi_infos(chassis_files)?,
+        })
     }
+    fn get_dmi_infos(files: Vec<&str>) -> Result<HashMap<String, String>> {
+        let mut res: HashMap<String, String> = HashMap::new();
 
-    Ok(res)
+        for file in files {
+            let path = format!("{DMI_PATH}/{file}");
+            let content = match fs::read_to_string(&path) {
+                Err(ref err) if err.kind() == std::io::ErrorKind::NotFound => continue,
+                Err(ref err) if err.kind() == std::io::ErrorKind::PermissionDenied => {
+                    bail!("Could not read data. Are you running as root or with sudo?")
+                }
+                Err(err) => bail!("Error: '{err}' on '{path}'"),
+                Ok(content) => content.trim().into(),
+            };
+            let key = file.splitn(2, '_').last().unwrap();
+            res.insert(key.into(), content);
+        }
+
+        Ok(res)
+    }
 }
