proxmox/pve-installer

commit a14a9348f2e931d0a0f8120093b5eb7b80a599fd

assistant: perpare-iso avoid separate directory and make less verbose
Signed-off-by: Thomas Lamprecht <t.lamprecht@proxmox.com>

proxmox-auto-install-assistant/src/main.rs
@@ -307,22 +307,25 @@ fn prepare_iso(args: &CommandPrepareISO) -> Result<()> {
     }
 
     let mut tmp_base = PathBuf::new();
-    if args.tmp.is_some() {
-        tmp_base.push(args.tmp.as_ref().unwrap());
-    } else {
-        tmp_base.push(args.source.parent().unwrap());
-        tmp_base.push(".proxmox-iso-prepare");
+    match args.tmp.as_ref() {
+        Some(tmp_dir) => tmp_base.push(tmp_dir),
+        None => tmp_base.push(args.source.parent().unwrap()),
     }
-    fs::create_dir_all(&tmp_base)?;
+
+    let iso_target = final_iso_location(args);
 
     let mut tmp_iso = tmp_base.clone();
-    tmp_iso.push("proxmox.iso");
+    let iso_target_file_name = match iso_target.file_name() {
+        None => bail!("no base filename in target ISO path found"),
+        Some(source_file_name) => source_file_name.to_string_lossy(),
+    };
+    tmp_iso.push(format!("{iso_target_file_name}.tmp",));
+
     let mut tmp_answer = tmp_base.clone();
     tmp_answer.push("answer.toml");
 
     println!("Copying source ISO to temporary location...");
     fs::copy(&args.source, &tmp_iso)?;
-    println!("Done copying source ISO");
 
     println!("Preparing ISO...");
     let install_mode = AutoInstSettings {
@@ -341,13 +344,9 @@ fn prepare_iso(args: &CommandPrepareISO) -> Result<()> {
         inject_file_to_iso(&tmp_iso, &tmp_answer, "/answer.toml")?;
     }
 
-    println!("Done preparing iso.");
-    println!("Move ISO to target location...");
-    let iso_target = final_iso_location(args);
+    println!("Moving prepared ISO to target location...");
     fs::rename(&tmp_iso, &iso_target)?;
-    println!("Cleaning up...");
-    fs::remove_dir_all(&tmp_base)?;
-    println!("Final ISO is available at {}.", &iso_target.display());
+    println!("Final ISO is available at {iso_target:?}.");
 
     Ok(())
 }

commit 938726d5003b4b25d2a2d69b5e1cdb61ced80977

assistant: prepare iso: avoid an useless intermediate copy of the answer file
This fixes a bug for the case where the answer file was named
"answer.toml", as then the code tried to copy the file to itself,
which just caused the file being truncated.

This was quite confusing for the user, as the validation of the answer
file happened before the copy step, so one would see that it got
correctly validated only to then turn up empty inside the ISO..

While this was introduced through a14a934 ("assistant: perpare-iso
avoid separate directory and make less verbose"), copying the answer
file before injecting it into the ISO was never required, so just use
the source file directly.

Note that this is still a bit racy w.r.t. validation and inserting
into the ISO, to fix that we'd need to pass around empty files or
write the content directly, both not worth it for now.

Fixes: a14a934
Reported-by: Stefan Hanreich <s.hanreich@proxmox.com>
Signed-off-by: Thomas Lamprecht <t.lamprecht@proxmox.com>

proxmox-auto-install-assistant/src/main.rs
@@ -324,9 +324,6 @@ fn prepare_iso(args: &CommandPrepareISO) -> Result<()> {
     };
     tmp_iso.push(format!("{iso_target_file_name}.tmp",));
 
-    let mut tmp_answer = tmp_base.clone();
-    tmp_answer.push("answer.toml");
-
     println!("Copying source ISO to temporary location...");
     fs::copy(&args.source, &tmp_iso)?;
 
@@ -342,9 +339,8 @@ fn prepare_iso(args: &CommandPrepareISO) -> Result<()> {
 
     inject_file_to_iso(&tmp_iso, &instmode_file_tmp, "/auto-installer-mode.toml")?;
 
-    if let Some(answer) = &args.answer_file {
-        fs::copy(answer, &tmp_answer)?;
-        inject_file_to_iso(&tmp_iso, &tmp_answer, "/answer.toml")?;
+    if let Some(answer_file) = &args.answer_file {
+        inject_file_to_iso(&tmp_iso, &answer_file, "/answer.toml")?;
     }
 
     println!("Moving prepared ISO to target location...");