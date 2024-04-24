b6b8dfd7e1773ef60432a253802f82f0d788fc96
Thomas Lamprecht <t.lamprecht@proxmox.com>
2024-04-24 11:24:10 +0200

    answer: replace custom display impl with derived one from serde plain
    Signed-off-by: Thomas Lamprecht <t.lamprecht@proxmox.com>

--- proxmox-auto-installer/src/answer.rs
+++ proxmox-auto-installer/src/answer.rs
@@ -301,9 +301,4 @@ pub enum KeyboardLayout {
     Tr,
 }
 
-impl std::fmt::Display for KeyboardLayout {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        let keyboard_layout = serde_json::to_value(self).unwrap().to_string();
-        write!(f, "{}", keyboard_layout.trim_matches('\"'))
-    }
-}
+serde_plain::derive_display_from_serialize!(KeyboardLayout);
