9e677e23802d96c4381707886513ece72f4679e2
Christoph Heiss <c.heiss@proxmox.com>
2024-11-11 14:15:05 +0100

    auto-installer: udevinfo: introduce type alias for udev properties
    Makes handling & passing them around a bit more convenient. Will be used
    in the upcoming proxmox-post-hook utility.
    
    Signed-off-by: Christoph Heiss <c.heiss@proxmox.com>

--- proxmox-auto-installer/src/udevinfo.rs
+++ proxmox-auto-installer/src/udevinfo.rs
@@ -1,9 +1,11 @@
 use serde::Deserialize;
 use std::collections::BTreeMap;
 
+/// Uses a BTreeMap to have the keys sorted
+pub type UdevProperties = BTreeMap<String, String>;
+
 #[derive(Clone, Deserialize, Debug)]
 pub struct UdevInfo {
-    // use BTreeMap to have keys sorted
-    pub disks: BTreeMap<String, BTreeMap<String, String>>,
-    pub nics: BTreeMap<String, BTreeMap<String, String>>,
+    pub disks: BTreeMap<String, UdevProperties>,
+    pub nics: BTreeMap<String, UdevProperties>,
 }
