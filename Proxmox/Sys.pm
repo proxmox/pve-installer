d531c68b2993280620764a23bbaa28bc65f6ae4c
Christoph Heiss <c.heiss@proxmox.com>
2024-12-16 10:41:00 +0100

    proxinstall: raise minimum root password length to 8 characters
    .. in accordance with current NIST recommendations [0].
    
    It's 2024; so reasonable to expect an 8-character-password at the
    minimum.
    
    [0] https://pages.nist.gov/800-63-4/sp800-63b.html#passwordver
    
    Signed-off-by: Christoph Heiss <c.heiss@proxmox.com>

--- Proxmox/Sys.pm
+++ Proxmox/Sys.pm
@@ -7,3 +7,8 @@ use warnings;
 # https://html.spec.whatwg.org/multipage/input.html#valid-e-mail-address
 # Using that /should/ cover all possible cases that are encountered in the wild.
 our $EMAIL_RE = '^[a-zA-Z0-9.!#$%&\'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$';
+
+# Minimum password length for the root account.
+# See also https://pages.nist.gov/800-63-4/sp800-63b.html#passwordver for the
+# recommendation.
+our $ROOT_PASSWORD_MIN_LENGTH = 8;
