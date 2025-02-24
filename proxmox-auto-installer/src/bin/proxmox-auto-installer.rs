proxmox/pve-installer

commit 21ef304897602340210d0c05844309bd96753340

unconfigured.sh: run proxmox-post-hook after successful auto-install
Signed-off-by: Christoph Heiss <c.heiss@proxmox.com>

unconfigured.sh
@@ -253,7 +253,10 @@ elif [ $start_auto_installer -ne 0 ]; then
         debugsh || true
     fi
     echo "Starting automatic installation"
-    /usr/bin/proxmox-auto-installer </run/automatic-installer-answers
+
+    if /usr/bin/proxmox-auto-installer </run/automatic-installer-answers; then
+	/usr/bin/proxmox-post-hook </run/automatic-installer-answers
+    fi
 else
     echo "Starting the installer GUI - see tty2 (CTRL+ALT+F2) for any errors..."
     xinit -- -dpi "$DPI" -s 0 >/dev/tty2 2>&1

commit 254bfaee87f55c91c5811b3dd93128f2fcb865ca

fix #5984: unconfigured: do not reboot if auto-installer fails by default
An unintended side effect was introduced in 21ef304, which caused the
auto-installer to reboot on error, even though the answer file option
`general.reboot_on_error` has been set to `false`.

When an error signal is received inside `unconfigured.sh` (after the
trap statement `trap 'err_reboot' ERR`), it causes any failing command
to execute the `err_reboot` function and therefore dropping the user in
the debug shell. One exception, as described under `trap` in bash(1), is
for a command being part of the test in an if statement. Therefore, if
the auto-installer failed, it won't trap to `err_reboot`.

Therefore, create a flag file "/run/proxmox-reboot-on-error" if the
auto-installer should reboot on error. If it should, the auto-installer
will be rebooted on error. If not, artificially trap to `err_reboot` to
drop into the debug shell.

The shell check at `err_reboot` can now be removed since the function is
called directly.

Fixes: 21ef304 ("unconfigured.sh: run proxmox-post-hook after successful auto-install")
Signed-off-by: Daniel Kral <d.kral@proxmox.com>

proxmox-auto-installer/src/bin/proxmox-auto-installer.rs
@@ -117,24 +117,19 @@ fn main() -> ExitCode {
         }
     };
 
-    match run_installation(&answer, &locales, &runtime_info, &udevadm_info, &setup_info) {
-        Ok(_) => info!("Installation done."),
-        Err(err) => {
-            error!("Installation failed: {err:#}");
-            return exit_failure(answer.global.reboot_on_error);
-        }
+    if answer.global.reboot_on_error {
+        let _ = File::create("/run/proxmox-reboot-on-error");
     }
 
-    ExitCode::SUCCESS
-}
-
-/// When we exit with a failure, the installer will not automatically reboot.
-/// Default value for reboot_on_error is false
-fn exit_failure(reboot_on_error: bool) -> ExitCode {
-    if reboot_on_error {
-        ExitCode::SUCCESS
-    } else {
-        ExitCode::FAILURE
+    match run_installation(&answer, &locales, &runtime_info, &udevadm_info, &setup_info) {
+        Ok(_) => {
+            info!("Installation done.");
+            ExitCode::SUCCESS
+        },
+        Err(err) => {
+            error!("Installation failed: {err:#}");
+            ExitCode::FAILURE
+        }
     }
 }
 

unconfigured.sh
@@ -104,7 +104,6 @@ real_reboot() {
 }
 
 # reachable through the ERR trap
-# shellcheck disable=SC2317
 err_reboot() {
     printf "\nInstallation aborted - unable to continue (type exit or CTRL-D to reboot)\n"
     debugsh || true
@@ -257,12 +256,21 @@ elif [ $start_auto_installer -ne 0 ]; then
     fi
     echo "Starting automatic installation"
 
+    # creates "/run/proxmox-reboot-on-error" if `global.reboot_on_error = true`
     if /usr/bin/proxmox-auto-installer </run/automatic-installer-answers; then
         if ! /usr/bin/proxmox-post-hook </run/automatic-installer-answers; then
             echo "post installation hook failed to execute."
-            echo "waiting 30s to allow gathering the error before reboot."
-            sleep 30
+
+	    # drop into debug shell if it shouldn't reboot on error
+            if [ ! -f /run/proxmox-reboot-on-error ]; then
+                err_reboot
+            else
+                echo "waiting 30s to allow gathering the error before reboot."
+                sleep 30
+            fi
         fi
+    elif [ ! -f /run/proxmox-reboot-on-error ]; then
+        err_reboot
     fi
 else
     echo "Starting the installer GUI - see tty2 (CTRL+ALT+F2) for any errors..."