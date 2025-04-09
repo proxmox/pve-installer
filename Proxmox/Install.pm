proxmox/pve-installer

commit dc5b8d7c6f4c90c59bf6a0f6d71cc43aa4f4834a

btrfs: use proxmox-boot-tool to sync ESPs for RAID installs
instead of just having the first one mounted.

This is done by handling it the same as ZFS regarding the ESP.

This fixes an issue where installing with BTRFS showed a 'Boot Option
Restoration' prompt after boot.

Signed-off-by: Dominik Csapak <d.csapak@proxmox.com>
Link: https://lore.proxmox.com/20250407132148.1051289-1-d.csapak@proxmox.com

Proxmox/Install.pm
@@ -1144,7 +1144,7 @@ sub extract_data {
 	# Note: this is required by current grub, but really dangerous, because
 	# vfat does not have journaling, so it triggers manual fsck after each crash
 	# so we only mount /boot/efi if really required (efi systems).
-	if ($run_env->{boot_type} eq 'efi' && !$use_zfs) {
+	if ($run_env->{boot_type} eq 'efi' && !($use_zfs || $use_btrfs)) {
 	    if (scalar(@$bootdevinfo)) {
 		my $di = @$bootdevinfo[0]; # simply use first disk
 
@@ -1368,7 +1368,7 @@ _EOD
 
 		foreach my $di (@$bootdevinfo) {
 		    my $dev = $di->{devname};
-		    if ($use_zfs) {
+		    if ($use_zfs || $use_btrfs) {
 			prepare_proxmox_boot_esp($di->{esp}, $targetdir, $run_env->{secure_boot});
 		    } else {
 			if (!$native_4k_disk_bootable) {

commit 268783c3e8f07dd6a30a8fdc18038a5f2681503d

btrfs: pass UUID of root file system to sd-boot via kernel cmdline
The recent change of handing the ESPs to proxmox-boot-tool broke
installation on btrfs booted with systemd-boot (currently efi-systems
without secure boot). our systemd-boot config-generation takes the
kernel command line from /etc/kernel/cmdline - without this there is
no root= parameter, and we end up in a initramfs shell.

Write the file unconditionally of boot-mode (legacy, efi,
efi-secureboot), to be consistent with the way we do it on ZFS.

The UUID is stored in a variable instead of running blkid multiple
time primarily to not end up with different versions of a similar
command in the future, at the expense of 3 change-sites which are
quite far apart.

Fixes: dc5b8d7 ("btrfs: use proxmox-boot-tool to sync ESPs for RAID installs")
Signed-off-by: Stoiko Ivanov <s.ivanov@proxmox.com>
Link: https://lore.proxmox.com/20250408191806.2410107-1-s.ivanov@proxmox.com
 [TL: rework commit message subject]
Signed-off-by: Thomas Lamprecht <t.lamprecht@proxmox.com>

Proxmox/Install.pm
@@ -751,6 +751,7 @@ sub extract_data {
 
     my ($swapfile, $rootdev, $datadev);
     my ($use_zfs, $use_btrfs) = (0, 0);
+    my $btrfs_uuid;
 
     my $filesys = Proxmox::Install::Config::get_filesys();
     my $hdsize = Proxmox::Install::Config::get_hdsize();
@@ -1125,6 +1126,9 @@ sub extract_data {
 
 	    die "unable to detect FS UUID" if !defined($fsuuid);
 
+	    # needed for /etc/kernel/cmdline when making the system bootable.
+	    $btrfs_uuid = $fsuuid;
+
 	    my $btrfs_opts = Proxmox::Install::Config::get_btrfs_opt();
 
 	    my $mountopts = 'defaults';
@@ -1331,6 +1335,11 @@ _EOD
 	    file_write_all("$targetdir/etc/default/grub.d/zfs.cfg", $zfs_snippet);
 
 	    file_write_all("$targetdir/etc/kernel/cmdline", "root=ZFS=$zfs_pool_name/ROOT/$zfs_root_volume_name boot=zfs $target_cmdline\n");
+	}
+
+	if ($use_btrfs) {
+	    # add root= option to /etc/kernel/cmdline as well (for systemd-boot+btrfs)
+	    file_write_all("$targetdir/etc/kernel/cmdline", "root=UUID=$btrfs_uuid $target_cmdline\n");
 	}
 
 	# Always write zfs module parameter - even if the user did not select ZFS-on-root.