proxmox/pve-installer

commit 2557dbff7bdcb9a2ef20281fcf101f23ad3ac004

common: initial upgrade to ureq 3 and rustls 0.23
incomplete as TLS cert validation needs to be fixed, we now need to
implement our own transport to be able to pass a rustls ClientConfig,
see an example here [0]. FWIW, ureq is evaluating if the verification
for cert fingerprints is important enough for ureq's main focus and
use cases to have better native support [1], so this has some chance
to be come simpler again in the future.

but update the rest to be prepared for that already.

[0]: https://github.com/algesten/ureq/pull/1085/files
[1]: https://github.com/algesten/ureq/issues/1087#issuecomment-2984113154

Signed-off-by: Thomas Lamprecht <t.lamprecht@proxmox.com>

proxmox-installer-common/src/http.rs
@@ -1,9 +1,10 @@
 use anyhow::Result;
 use rustls::ClientConfig;
+use rustls::pki_types::{CertificateDer, ServerName, UnixTime};
 use sha2::{Digest, Sha256};
 use std::io::Read;
 use std::sync::Arc;
-use ureq::{Agent, AgentBuilder};
+use ureq::Agent;
 
 /// Builds an [`Agent`] with TLS suitable set up, depending whether a custom fingerprint was
 /// supplied or not. If a fingerprint was supplied, only matching certificates will be accepted.
@@ -19,26 +20,24 @@ use ureq::{Agent, AgentBuilder};
 fn build_agent(fingerprint: Option<&str>) -> Result<Agent> {
     if let Some(fingerprint) = fingerprint {
         let tls_config = ClientConfig::builder()
-            .with_safe_defaults()
+            .dangerous()
             .with_custom_certificate_verifier(VerifyCertFingerprint::new(fingerprint)?)
             .with_no_client_auth();
 
-        Ok(AgentBuilder::new().tls_config(Arc::new(tls_config)).build())
+        Ok(Agent::config_builder()
+            //.tls_config(tls_config) // FIXME: add custom ureq connector to manage rustls
+            .build()
+            .into())
     } else {
-        let mut roots = rustls::RootCertStore::empty();
-        for cert in rustls_native_certs::load_native_certs()? {
-            roots.add(&rustls::Certificate(cert.0)).unwrap();
-        }
-
-        let tls_config = rustls::ClientConfig::builder()
-            .with_safe_defaults()
-            .with_root_certificates(roots)
-            .with_no_client_auth();
-
-        Ok(AgentBuilder::new()
-            .tls_connector(Arc::new(native_tls::TlsConnector::new()?))
-            .tls_config(Arc::new(tls_config))
-            .build())
+        Ok(Agent::config_builder()
+            .timeout_global(Some(std::time::Duration::from_secs(60)))
+            .tls_config(
+                ureq::tls::TlsConfig::builder()
+                    .root_certs(ureq::tls::RootCerts::PlatformVerifier)
+                    .build(),
+            )
+            .build()
+            .into())
     }
 }
 
@@ -58,12 +57,9 @@ fn build_agent(fingerprint: Option<&str>) -> Result<Agent> {
 pub fn get_as_bytes(url: &str, fingerprint: Option<&str>, max_size: usize) -> Result<Vec<u8>> {
     let mut result: Vec<u8> = Vec::new();
 
-    let response = build_agent(fingerprint)?
-        .get(url)
-        .timeout(std::time::Duration::from_secs(60))
-        .call()?;
+    let (_, body) = build_agent(fingerprint)?.get(url).call()?.into_parts();
 
-    response
+    body
         .into_reader()
         .take(max_size as u64)
         .read_to_end(&mut result)?;
@@ -83,14 +79,16 @@ pub fn get_as_bytes(url: &str, fingerprint: Option<&str>, max_size: usize) -> Re
 /// * `fingerprint` - SHA256 cert fingerprint if certificate pinning should be used. Optional.
 /// * `payload` - The payload to send to the server. Expected to be a JSON formatted string.
 pub fn post(url: &str, fingerprint: Option<&str>, payload: String) -> Result<String> {
+    // TODO: read_to_string limits the size to 10 MB, should be increase that?
     Ok(build_agent(fingerprint)?
         .post(url)
-        .set("Content-Type", "application/json; charset=utf-8")
-        .timeout(std::time::Duration::from_secs(60))
-        .send_string(&payload)?
-        .into_string()?)
+        .header("Content-Type", "application/json; charset=utf-8")
+        .send(&payload)?
+        .body_mut()
+        .read_to_string()?)
 }
 
+#[derive(Debug)]
 struct VerifyCertFingerprint {
     cert_fingerprint: Vec<u8>,
 }
@@ -106,24 +104,57 @@ impl VerifyCertFingerprint {
     }
 }
 
-impl rustls::client::ServerCertVerifier for VerifyCertFingerprint {
+impl rustls::client::danger::ServerCertVerifier for VerifyCertFingerprint {
     fn verify_server_cert(
         &self,
-        end_entity: &rustls::Certificate,
-        _intermediates: &[rustls::Certificate],
-        _server_name: &rustls::ServerName,
-        _scts: &mut dyn Iterator<Item = &[u8]>,
+        end_entity: &CertificateDer,
+        _intermediates: &[CertificateDer],
+        _server_name: &ServerName,
         _ocsp_response: &[u8],
-        _now: std::time::SystemTime,
-    ) -> Result<rustls::client::ServerCertVerified, rustls::Error> {
+        _now: UnixTime,
+    ) -> Result<rustls::client::danger::ServerCertVerified, rustls::Error> {
         let mut hasher = Sha256::new();
         hasher.update(end_entity);
         let result = hasher.finalize();
 
         if result.as_slice() == self.cert_fingerprint {
-            Ok(rustls::client::ServerCertVerified::assertion())
+            Ok(rustls::client::danger::ServerCertVerified::assertion())
         } else {
             Err(rustls::Error::General("Fingerprint did not match!".into()))
         }
+    }
+
+    fn verify_tls12_signature(
+        &self,
+        message: &[u8],
+        cert: &CertificateDer<'_>,
+        dss: &rustls::DigitallySignedStruct,
+    ) -> Result<rustls::client::danger::HandshakeSignatureValid, rustls::Error> {
+        rustls::crypto::verify_tls12_signature(
+            message,
+            cert,
+            dss,
+            &rustls::crypto::ring::default_provider().signature_verification_algorithms,
+        )
+    }
+
+    fn verify_tls13_signature(
+        &self,
+        message: &[u8],
+        cert: &CertificateDer<'_>,
+        dss: &rustls::DigitallySignedStruct,
+    ) -> Result<rustls::client::danger::HandshakeSignatureValid, rustls::Error> {
+        rustls::crypto::verify_tls13_signature(
+            message,
+            cert,
+            dss,
+            &rustls::crypto::ring::default_provider().signature_verification_algorithms,
+        )
+    }
+
+    fn supported_verify_schemes(&self) -> Vec<rustls::SignatureScheme> {
+        rustls::crypto::ring::default_provider()
+            .signature_verification_algorithms
+            .supported_schemes()
     }
 }

debian/control
@@ -17,14 +17,14 @@ Build-Depends: cargo:native,
                librust-pico-args-0.5-dev,
                librust-pretty-assertions-1.4-dev,
                librust-regex-1+default-dev (>= 1.7~~),
-               librust-rustls-0.21+dangerous-configuration-dev,
+               librust-rustls-0.23-dev,
                librust-rustls-native-certs-dev,
                librust-serde-1+default-dev,
                librust-serde-json-1+default-dev,
                librust-serde-plain-1+default-dev,
                librust-sha2-0.10-dev,
                librust-toml-0.8-dev,
-               librust-ureq-2.10-dev,
+               librust-ureq-3-dev,
                libtest-mockmodule-perl,
                patchelf,
                perl,

proxmox-installer-common/Cargo.toml
@@ -17,10 +17,10 @@ serde_plain.workspace = true
 # `http` feature
 hex = { version = "0.4", optional = true }
 native-tls = { version = "0.2", optional = true }
-rustls = { version = "0.21", features = [ "dangerous_configuration" ], optional = true }
+rustls = { version = "0.23", optional = true }
 rustls-native-certs = { version = "0.6", optional = true }
 sha2 = { version = "0.10", optional = true }
-ureq = { version = "2.6", features = [ "native-certs", "native-tls" ], optional = true }
+ureq = { version = "3", features = [ "platform-verifier"  ], optional = true }
 
 # `cli` feature
 pico-args = { workspace = true, optional = true }

commit b877bc184bf43156da1daeaf0fe05bf826aea1e1

common: http: add custom ureq connector for cert-fingerprint validation
Adds back custom TLS fingerprint validation for ureq 3. The API was
changed majorly, no longer providing the possibility to directly provide
a rustls `ClientConfig` to the agent.

Instead, a complete custom transport + connector must be provided to
ureq to achieve this.

Mostly based on ureq#1085 [0], which provides a example how to implement
something like this.

[0] https://github.com/algesten/ureq/pull/1085

Fixes: 2557dbf ("common: initial upgrade to ureq 3 and rustls 0.23")
Signed-off-by: Christoph Heiss <c.heiss@proxmox.com>
Link: https://lore.proxmox.com/20250625122229.1115619-1-c.heiss@proxmox.com
Signed-off-by: Thomas Lamprecht <t.lamprecht@proxmox.com>

proxmox-installer-common/src/http.rs
@@ -1,10 +1,17 @@
 use anyhow::Result;
-use rustls::ClientConfig;
 use rustls::pki_types::{CertificateDer, ServerName, UnixTime};
+use rustls::{ClientConfig, ClientConnection, StreamOwned};
 use sha2::{Digest, Sha256};
-use std::io::Read;
+use std::fmt;
+use std::io::{Read, Write};
 use std::sync::Arc;
+use std::time::Duration;
 use ureq::Agent;
+use ureq::unversioned::resolver::DefaultResolver;
+use ureq::unversioned::transport::{
+    Buffers, ConnectionDetails, Connector, Either, LazyBuffers, NextTimeout, TcpConnector,
+    Transport, TransportAdapter,
+};
 
 /// Builds an [`Agent`] with TLS suitable set up, depending whether a custom fingerprint was
 /// supplied or not. If a fingerprint was supplied, only matching certificates will be accepted.
@@ -18,19 +25,34 @@ use ureq::Agent;
 /// # Arguments
 /// * `fingerprint` - SHA256 cert fingerprint if certificate pinning should be used. Optional.
 fn build_agent(fingerprint: Option<&str>) -> Result<Agent> {
+    const GLOBAL_TIMEOUT: Duration = Duration::from_secs(60);
+
     if let Some(fingerprint) = fingerprint {
-        let tls_config = ClientConfig::builder()
+        // If the user specified a custom TLS fingerprint, we must use a custom
+        // `rustls::ClientConfig`, which in turns means to use a custom
+        // `Connector`.
+        let crypto_provider = rustls::crypto::CryptoProvider::get_default()
+            .cloned()
+            .unwrap_or_else(|| Arc::new(rustls::crypto::ring::default_provider()));
+
+        let tls_config = ClientConfig::builder_with_provider(crypto_provider)
+            .with_protocol_versions(rustls::ALL_VERSIONS)?
             .dangerous()
             .with_custom_certificate_verifier(VerifyCertFingerprint::new(fingerprint)?)
             .with_no_client_auth();
 
-        Ok(Agent::config_builder()
-            //.tls_config(tls_config) // FIXME: add custom ureq connector to manage rustls
-            .build()
-            .into())
+        let connector = UreqRustlsConnector::new(Arc::new(tls_config));
+
+        Ok(Agent::with_parts(
+            ureq::config::Config::builder()
+                .timeout_global(Some(GLOBAL_TIMEOUT))
+                .build(),
+            TcpConnector::default().chain(connector),
+            DefaultResolver::default(),
+        ))
     } else {
         Ok(Agent::config_builder()
-            .timeout_global(Some(std::time::Duration::from_secs(60)))
+            .timeout_global(Some(GLOBAL_TIMEOUT))
             .tls_config(
                 ureq::tls::TlsConfig::builder()
                     .root_certs(ureq::tls::RootCerts::PlatformVerifier)
@@ -59,8 +81,7 @@ pub fn get_as_bytes(url: &str, fingerprint: Option<&str>, max_size: usize) -> Re
 
     let (_, body) = build_agent(fingerprint)?.get(url).call()?.into_parts();
 
-    body
-        .into_reader()
+    body.into_reader()
         .take(max_size as u64)
         .read_to_end(&mut result)?;
 
@@ -156,5 +177,109 @@ impl rustls::client::danger::ServerCertVerifier for VerifyCertFingerprint {
         rustls::crypto::ring::default_provider()
             .signature_verification_algorithms
             .supported_schemes()
+    }
+}
+
+/// Mostly a copy of [ureq::unversioned::transport::RustlsConnector], with the exception of using
+/// our custom [ClientConfig].
+#[derive(Debug)]
+struct UreqRustlsConnector {
+    /// [ClientConfig] to use for the TLS connection(s).
+    config: Arc<ClientConfig>,
+}
+
+impl UreqRustlsConnector {
+    fn new(config: Arc<ClientConfig>) -> Self {
+        UreqRustlsConnector { config }
+    }
+}
+
+impl<In: Transport> Connector<In> for UreqRustlsConnector {
+    type Out = Either<In, UreqRustlsTransport>;
+
+    fn connect(
+        &self,
+        details: &ConnectionDetails,
+        chained: Option<In>,
+    ) -> Result<Option<Self::Out>, ureq::Error> {
+        let Some(transport) = chained else {
+            panic!("RustlConnector requires a chained transport");
+        };
+
+        if !details.needs_tls() || transport.is_tls() {
+            return Ok(Some(Either::A(transport)));
+        }
+
+        let name: ServerName<'_> = details
+            .uri
+            .authority()
+            .ok_or(ureq::Error::Tls("no naming authority for URI"))?
+            .host()
+            .try_into()
+            .map_err(|_| ureq::Error::Tls("invalid dns name"))?;
+
+        let conn = ClientConnection::new(self.config.clone(), name.to_owned())?;
+        let stream = StreamOwned {
+            conn,
+            sock: TransportAdapter::new(transport.boxed()),
+        };
+
+        let buffers = LazyBuffers::new(
+            details.config.input_buffer_size(),
+            details.config.output_buffer_size(),
+        );
+
+        let transport = UreqRustlsTransport { buffers, stream };
+
+        Ok(Some(Either::B(transport)))
+    }
+}
+
+/// Direct copy of ureq/tls/rustls.rs:RustlsTransport, which unfortunately is not
+/// made public by the crate.
+struct UreqRustlsTransport {
+    buffers: LazyBuffers,
+    stream: StreamOwned<ClientConnection, TransportAdapter>,
+}
+
+impl Transport for UreqRustlsTransport {
+    fn buffers(&mut self) -> &mut dyn Buffers {
+        &mut self.buffers
+    }
+
+    fn transmit_output(&mut self, amount: usize, timeout: NextTimeout) -> Result<(), ureq::Error> {
+        self.stream.get_mut().set_timeout(timeout);
+
+        let output = &self.buffers.output()[..amount];
+        self.stream.write_all(output)?;
+
+        Ok(())
+    }
+
+    fn await_input(&mut self, timeout: NextTimeout) -> Result<bool, ureq::Error> {
+        self.stream.get_mut().set_timeout(timeout);
+
+        let input = self.buffers.input_append_buf();
+        let amount = self.stream.read(input)?;
+        self.buffers.input_appended(amount);
+
+        Ok(amount > 0)
+    }
+
+    fn is_open(&mut self) -> bool {
+        self.stream.get_mut().get_mut().is_open()
+    }
+
+    fn is_tls(&self) -> bool {
+        true
+    }
+}
+
+impl fmt::Debug for UreqRustlsTransport {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_struct("RustlsTransport")
+            .field("chained", &self.stream.sock.inner())
+            .field("buffers", &self.buffers)
+            .finish()
     }
 }