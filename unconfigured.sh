proxmox/pve-installer

commit dc36013cf97bba0bd835bfed5d815c32ebaba162

unconfigured: rework stopping systemd-udevd slightly
Signed-off-by: Thomas Lamprecht <t.lamprecht@proxmox.com>

unconfigured.sh
@@ -81,11 +81,16 @@ real_reboot() {
 
     # stop udev (release file handles)
     udevd_pid="$(pgrep systemd-udevd)"
-    if kill -s TERM "$udevd_pid"; then
-        if ! waitpid --exited --timeout 5 "$udevd_pid"; then
-            echo "failed to wait for udevd exit - $?"
-            kill -s KILL "$udevd_pid" || :
+    if -n "$udevd_pid"; then
+        if kill -s TERM "$udevd_pid"; then
+            if ! waitpid --exited --timeout 5 "$udevd_pid"; then
+                echo "failed to wait for udevd exit - $?"
+                kill -s KILL "$udevd_pid" || :
+            fi
         fi
+    else
+        # cannot hurt I guess..
+        pkill systemd-udevd || :
     fi
 
     swap=$(awk '/^\/dev\// { print $1 }' /proc/swaps);

commit 190a64e5dd300a9192dfe32d2f28b3589961652c

unconfigured: add missing [ ] around if clause
"-n" would be treated like a command here otherwise.

Fixes: dc36013 ("unconfigured: rework stopping systemd-udevd slightly")
Signed-off-by: Christoph Heiss <c.heiss@proxmox.com>
Link: https://lore.proxmox.com/20250626075052.65257-1-c.heiss@proxmox.com

unconfigured.sh
@@ -81,7 +81,7 @@ real_reboot() {
 
     # stop udev (release file handles)
     udevd_pid="$(pgrep systemd-udevd)"
-    if -n "$udevd_pid"; then
+    if [ -n "$udevd_pid" ]; then
         if kill -s TERM "$udevd_pid"; then
             if ! waitpid --exited --timeout 5 "$udevd_pid"; then
                 echo "failed to wait for udevd exit - $?"