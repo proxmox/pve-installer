proxmox/pve-installer

commit a37e0442b4bd2a5f3ce6fd65e955620285101931

fix #5811: auto: add option to retrieve FQDN from DHCP configuration
Fixes #5811 [0].

Adds a new option to the answer file for specifying to use the
DHCP-provided hostname and domain. For the domain, an additional
fallback/default can be specified, in case the DHCP server only provides
hostnames. The hostname is always required in this mode.

The addition to the answer file format is done in a backwards-compatible
way.

Users can now either directly specify the desired FQDN in the answer
file as before through

```
[global]
fqdn = "node.example.local"
```

or, with the new mechanism, if the DHCP server provides all the
information already:

```
[global]
fqdn.source = "from-dhcp"
```

or additionally with a custom domain:

```
[global.fqdn]
source = "from-dhcp"
domain = "custom.domain.local"
```

The DHCP-provided hostname and domain are already provided to the
auto-installer through the runtime environment, so its just a matter of
using them.

The test suite is extended quite a bit, to cover all the different
cases.

[0] https://bugzilla.proxmox.com/show_bug.cgi?id=5811

Signed-off-by: Christoph Heiss <c.heiss@proxmox.com>
 [TL: resolve conflict from context of recent ZFS related changes]
Signed-off-by: Thomas Lamprecht <t.lamprecht@proxmox.com>

proxmox-auto-installer/src/answer.rs
@@ -46,7 +46,8 @@ impl Answer {
 #[serde(rename_all = "kebab-case", deny_unknown_fields)]
 pub struct Global {
     pub country: String,
-    pub fqdn: Fqdn,
+    /// FQDN to set for the installed system.
+    pub fqdn: FqdnConfig,
     pub keyboard: KeyboardLayout,
     pub mailto: String,
     pub timezone: String,
@@ -68,6 +69,39 @@ pub enum RebootMode {
     #[default]
     Reboot,
     PowerOff,
+}
+
+/// Allow the user to either set the FQDN of the installation to either some
+/// fixed value or retrieve it dynamically via e.g.DHCP.
+#[derive(Clone, Deserialize, Debug)]
+#[serde(
+    untagged,
+    expecting = "either a fully-qualified domain name or extendend configuration for usage with DHCP must be specified"
+)]
+pub enum FqdnConfig {
+    /// Sets the FQDN to the exact value.
+    Simple(Fqdn),
+    /// Extended configuration, e.g. to use hostname and domain from DHCP.
+    Extended(FqdnExtendedConfig),
+}
+
+/// Extended configuration for retrieving the FQDN from external sources.
+#[derive(Clone, Deserialize, Debug)]
+#[serde(rename_all = "kebab-case", deny_unknown_fields)]
+pub struct FqdnExtendedConfig {
+    /// Source to gather the FQDN from.
+    #[serde(default)]
+    pub source: FqdnSourceMode,
+    /// Domain to use if none is received via DHCP.
+    pub domain: Option<String>,
+}
+
+/// Describes the source to retrieve the FQDN of the installation.
+#[derive(Clone, Deserialize, Debug, Default, PartialEq)]
+#[serde(rename_all = "kebab-case", deny_unknown_fields)]
+pub enum FqdnSourceMode {
+    #[default]
+    FromDhcp,
 }
 
 #[derive(Clone, Deserialize, Debug)]

proxmox-auto-installer/tests/parse-answer.rs
@@ -116,12 +116,16 @@ mod tests {
 
         declare_tests!(
             run_named_test,
+            // Keep below entries alphabetically sorted
             btrfs,
             btrfs_raid_level_uppercase,
             disk_match,
             disk_match_all,
             disk_match_any,
             first_boot,
+            fqdn_from_dhcp,
+            fqdn_from_dhcp_no_dhcp_domain_with_default_domain,
+            full_fqdn_from_dhcp_with_default_domain,
             hashed_root_password,
             minimal,
             nic_matching,
@@ -136,7 +140,11 @@ mod tests {
 
         declare_tests!(
             run_named_fail_parse_test,
+            // Keep below entries alphabetically sorted
             both_password_and_hashed_set,
+            fqdn_from_dhcp_no_default_domain,
+            fqdn_hostname_only,
+            no_fqdn_from_dhcp,
             no_root_password_set,
             short_password
         );

proxmox-auto-installer/src/utils.rs
@@ -5,7 +5,9 @@ use log::info;
 use std::{collections::BTreeMap, process::Command};
 
 use crate::{
-    answer::{self, Answer, FirstBootHookSourceMode},
+    answer::{
+        self, Answer, FirstBootHookSourceMode, FqdnConfig, FqdnExtendedConfig, FqdnSourceMode,
+    },
     udevinfo::UdevInfo,
 };
 use proxmox_installer_common::{
@@ -24,12 +26,37 @@ fn get_network_settings(
     runtime_info: &RuntimeInfo,
     setup_info: &SetupInfo,
 ) -> Result<NetworkOptions> {
-    let mut network_options = NetworkOptions::defaults_from(setup_info, &runtime_info.network, None);
+    info!("Setting up network configuration");
 
-    info!("Setting network configuration");
+    let mut network_options = match &answer.global.fqdn {
+        // If the user set a static FQDN in the answer file, override it
+        FqdnConfig::Simple(name) => {
+            let mut opts = NetworkOptions::defaults_from(setup_info, &runtime_info.network, None);
+            opts.fqdn = name.to_owned();
+            opts
+        }
+        FqdnConfig::Extended(FqdnExtendedConfig {
+            source: FqdnSourceMode::FromDhcp,
+            domain,
+        }) => {
+            // A FQDN from DHCP information and/or defaults is constructed in
+            // `NetworkOptions::defaults_from()` below, just check that the DHCP server actually
+            // provided a hostname.
+            if runtime_info.network.hostname.is_none() {
+                bail!(
+                    "`global.fqdn.source` set to \"from-dhcp\", but DHCP server did not provide a hostname!"
+                );
+            }
 
-    // Always use the FQDN from the answer file
-    network_options.fqdn = answer.global.fqdn.clone();
+            // Either a domain must be received from the DHCP server or it must be set manually
+            // (even just as fallback) in the answer file.
+            if runtime_info.network.dns.domain.is_none() && domain.is_none() {
+                bail!("no domain received from DHCP server and `global.fqdn.domain` is unset!");
+            }
+
+            NetworkOptions::defaults_from(setup_info, &runtime_info.network, domain.as_deref())
+        }
+    };
 
     if let answer::NetworkSettings::Manual(settings) = &answer.network.network_settings {
         network_options.address = settings.cidr.clone();

proxmox-auto-installer/tests/resources/parse_answer/fqdn_from_dhcp.toml
@@ -1,0 +1,14 @@
+[global]
+keyboard = "de"
+country = "at"
+fqdn.source = "from-dhcp"
+mailto = "mail@no.invalid"
+timezone = "Europe/Vienna"
+root_password = "12345678"
+
+[network]
+source = "from-dhcp"
+
+[disk-setup]
+filesystem = "ext4"
+disk_list = ["sda"]

proxmox-auto-installer/tests/resources/parse_answer/fqdn_from_dhcp_no_dhcp_domain_with_default_domain.toml
@@ -1,0 +1,17 @@
+[global]
+keyboard = "de"
+country = "at"
+mailto = "mail@no.invalid"
+timezone = "Europe/Vienna"
+root_password = "12345678"
+
+[global.fqdn]
+source = "from-dhcp"
+domain = "custom.local"
+
+[network]
+source = "from-dhcp"
+
+[disk-setup]
+filesystem = "ext4"
+disk_list = ["sda"]

proxmox-auto-installer/tests/resources/parse_answer/full_fqdn_from_dhcp_with_default_domain.toml
@@ -1,0 +1,17 @@
+[global]
+keyboard = "de"
+country = "at"
+mailto = "mail@no.invalid"
+timezone = "Europe/Vienna"
+root_password = "12345678"
+
+[global.fqdn]
+source = "from-dhcp"
+domain = "custom.local"
+
+[network]
+source = "from-dhcp"
+
+[disk-setup]
+filesystem = "ext4"
+disk_list = ["sda"]

proxmox-auto-installer/tests/resources/parse_answer_fail/fqdn_from_dhcp_no_default_domain.toml
@@ -1,0 +1,14 @@
+[global]
+keyboard = "de"
+country = "at"
+fqdn.source = "from-dhcp"
+mailto = "mail@no.invalid"
+timezone = "Europe/Vienna"
+root_password = "12345678"
+
+[network]
+source = "from-dhcp"
+
+[disk-setup]
+filesystem = "ext4"
+disk_list = ["sda"]

proxmox-auto-installer/tests/resources/parse_answer_fail/fqdn_hostname_only.toml
@@ -1,0 +1,14 @@
+[global]
+keyboard = "de"
+country = "at"
+fqdn = "foobar"
+mailto = "mail@no.invalid"
+timezone = "Europe/Vienna"
+root_password = "12345678"
+
+[network]
+source = "from-dhcp"
+
+[disk-setup]
+filesystem = "ext4"
+disk_list = ["sda"]

commit 3d81300ca62246989bc18991c85a67d1234b1a97

auto: answer: deserialize empty domain name as `None`
This handles the case where users set `global.fqdn.domain = ""`, which
would result incorrect parsing and finally resulting in the FQDN of the
target machine being set to `<productname>.example.invalid`.

Reported on the community forum [0].

[0] https://forum.proxmox.com/threads/auto-install-fetching-fqdn-through-dhcp-does-not-set-search-domain-correctly.168369/

Fixes: a37e044 ("fix #5811: auto: add option to retrieve FQDN from DHCP configuration")
Signed-off-by: Christoph Heiss <c.heiss@proxmox.com>
Link: https://lore.proxmox.com/20250715135620.1023136-3-c.heiss@proxmox.com

proxmox-auto-installer/src/answer.rs
@@ -90,6 +90,7 @@ pub struct FqdnExtendedConfig {
     #[serde(default)]
     pub source: FqdnSourceMode,
     /// Domain to use if none is received via DHCP.
+    #[serde(default, deserialize_with = "deserialize_non_empty_string_maybe")]
     pub domain: Option<String>,
 }
 
@@ -444,3 +445,15 @@ pub enum KeyboardLayout {
 }
 
 serde_plain::derive_display_from_serialize!(KeyboardLayout);
+
+fn deserialize_non_empty_string_maybe<'de, D>(deserializer: D) -> Result<Option<String>, D::Error>
+where
+    D: serde::Deserializer<'de>,
+{
+    let val: Option<String> = Deserialize::deserialize(deserializer)?;
+
+    match val {
+        Some(s) if !s.is_empty() => Ok(Some(s)),
+        _ => Ok(None),
+    }
+}

proxmox-auto-installer/tests/parse-answer.rs
@@ -124,6 +124,7 @@ mod tests {
             disk_match_any,
             first_boot,
             fqdn_from_dhcp,
+            fqdn_from_dhcp_empty_dhcp_domain_setting,
             fqdn_from_dhcp_no_dhcp_domain_with_default_domain,
             full_fqdn_from_dhcp_with_default_domain,
             hashed_root_password,

proxmox-auto-installer/tests/resources/parse_answer/fqdn_from_dhcp_empty_dhcp_domain_setting.toml
@@ -1,0 +1,17 @@
+[global]
+keyboard = "de"
+country = "at"
+mailto = "mail@no.invalid"
+timezone = "Europe/Vienna"
+root-password = "12345678"
+
+[global.fqdn]
+source = "from-dhcp"
+domain = ""
+
+[network]
+source = "from-dhcp"
+
+[disk-setup]
+filesystem = "ext4"
+disk-list = ["sda"]