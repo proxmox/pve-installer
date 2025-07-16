proxmox/pve-installer

commit a9c0a445ab7cc7848e5e6271365cdd69abd9eb03

run env: fix dhcp-set hostname containing local domain
According to RFC 2132 section 3.14 [0], the hostname _can_ also include the
local domain name. Strip the suffix it before storing it into the
runtime environment.

Reported on the community forum [1].

[0] https://www.rfc-editor.org/rfc/rfc2132#section-3.14
[1] https://forum.proxmox.com/threads/auto-install-fetching-fqdn-through-dhcp-does-not-set-search-domain-correctly.168369/

Fixes: bda1cdf ("run env: retrieve and store hostname from DHCP lease if available")
Signed-off-by: Christoph Heiss <c.heiss@proxmox.com>
Link: https://lore.proxmox.com/20250715135620.1023136-2-c.heiss@proxmox.com

Proxmox/Install/RunEnv.pm
@@ -292,7 +292,12 @@ sub query_installation_environment : prototype() {
     };
 
     # avoid serializing out null or an empty string, that can trip up the UIs
-    if (my $fqdn = Proxmox::Sys::Net::get_dhcp_fqdn()) {
+    if (my $fqdn = Proxmox::Sys::Net::get_dhcp_hostname()) {
+        if (defined($output->{network}->{dns}->{domain})) {
+            # strip domain name suffix, if possible
+            $fqdn =~ s/\.$output->{network}->{dns}->{domain}//;
+        }
+
         $output->{network}->{hostname} = $fqdn;
     }
 

Proxmox/Sys/Net.pm
@@ -204,12 +204,16 @@ sub udevadm_netdev_details {
     return $result;
 }
 
-# Tries to detect the FQDN hostname for this system via DHCP, if available.
+# Tries to detect the hostname for this system given via DHCP, if available.
+# The hostname _might_ also include the local domain name, depending on the
+# concrete DHCP server implementation.
 #
-# DHCP server can set option 12 to inform the client about it's hostname [0]. dhclient dumps all
-# options set by the DHCP server it in lease file, so just read it from there.
+# DHCP servers can set option 12 to inform the client about it's hostname [0].
+# dhclient dumps all options set by the DHCP server it in lease file, so just
+# read it from there.
+#
 # [0] RFC 2132, section 3.14
-sub get_dhcp_fqdn : prototype() {
+sub get_dhcp_hostname : prototype() {
     my $leasefile = '/var/lib/dhcp/dhclient.leases';
     return if !-f $leasefile;
 

commit e4c1c06e8742de69095b2d7bafa74942c112a349

run env: anchor domain suffix strip regex to end of string
While it is very much an edge case, properly anchoring the regex ensures
that the local domain only ever gets stripped if it is indeed a suffix
of the fully-qualified hostname.

Prevents situations where the DHCP server sends the following client
configuration:

  host-name: host.foo.bar
  domain-name: foo

Which would result in `bar.foo` being set as the search domain in the
final system without this patch.

Fixes: a9c0a44 ("run env: fix dhcp-set hostname containing local domain")
Signed-off-by: Christoph Heiss <c.heiss@proxmox.com>
Link: https://lore.proxmox.com/20250716101320.571502-1-c.heiss@proxmox.com

Proxmox/Install/RunEnv.pm
@@ -296,7 +296,7 @@ sub query_installation_environment : prototype() {
     if (my $fqdn = Proxmox::Sys::Net::get_dhcp_hostname()) {
         if (defined(my $domain = $output->{network}->{dns}->{domain})) {
             # strip domain name suffix, if possible
-            $fqdn =~ s/\.${domain}//;
+            $fqdn =~ s/\.${domain}$//;
         }
 
         $output->{network}->{hostname} = $fqdn;